b7fc11dd6eab7c378f041ae3b3cc8901a39456ed
Pearyman <liyananhappy@sina.cn>
2015-02-08 12:12:06 +0800

    update

--- gitgreen.js
+++ gitgreen.js
@@ -11,7 +11,7 @@
 			var fs=require('fs');
 			var child_process=require('child_process');
 			// var currentTime=parseInt(new Date().getTime().toString().substr(0,10));
-			var currentTime=1424923926;
+			var currentTime=1423455126;
 			var lastTime=currentTime-365*24*60*60;
 			setInterval(function(){
 				currentTime=parseInt(currentTime-86400);
@@ -29,7 +29,7 @@
 
 			 })
 
-			},16000);
+			},15000);
 			
 		 	
  
