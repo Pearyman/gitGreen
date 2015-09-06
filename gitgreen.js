131667bba0284e3355981d8858cda74e5cd8d27f
Pearyman <liyananhappy@sina.cn>
2015-09-06 20:23:08 +0800

    update

---
+++ gitgreen.js
@@ -0,0 +1,25 @@
+/*
+ * Developer : Yanan Li (liyananhappy@hotmail.com)
+ * Date : 06/09/2015
+ *
+ * Copyright 2015
+ * Licensed under MIT
+ *
+ * Version: 1.00
+ */
+
+ var fs=require('fs');
+ var child_process=require('child_process');
+ var currentTime=parseInt(new Date().getTime().toString().substr(0,10));
+ fs.writeFile('gitgreen.txt',currentTime,function(err){
+ 	if(err){
+ 		console.log('write failed!');
+ 	}else{
+ 		console.log('write succeed');
+ 		child_process.exec("git add -A;GIT_AUTHOR_DATE='" + currentTime + "' GIT_COMMITTER_DATE='" + currentTime + "' git commit -m 'update'; git push;",function(err,opt){
+ 			if(err) throw err;
+ 			console.log("done!!");
+ 		})
+ 	}
+
+ })
