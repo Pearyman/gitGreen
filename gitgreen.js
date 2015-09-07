b3c9b45b4ed55dc45bbae01181d87a4130802fd3
Pearyman <liyananhappy@sina.cn>
2015-09-07 11:55:17 +0800

    update

--- gitgreen.js
+++ gitgreen.js
@@ -13,11 +13,11 @@
 			var currentTime=parseInt(new Date().getTime().toString().substr(0,10));
 			var lastTime=currentTime-365*24*60*60;
 			setInterval(function(){
+				currentTime=parseInt(currentTime-86400);
 				fs.writeFile('gitgreen.txt',currentTime,function(err){
 			 	if(err){
 			 		console.log('write failed!');
 			 	}else{
-			 		currentTime=parseInt(currentTime-86400);
 			 		console.log('write succeed');
 			 		console.log(currentTime);
 			 		child_process.exec("git add .; GIT_AUTHOR_DATE='" + currentTime + "' GIT_COMMITTER_DATE='" + currentTime + "';git commit -m 'update'; git push origin master;",function(err,opt){
