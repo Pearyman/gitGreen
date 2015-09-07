a4d1b515fe72447ad66f9ed0e739611175f91ebc
Pearyman <liyananhappy@sina.cn>
2015-09-07 11:14:33 +0800

    udate

--- gitgreen.js
+++ gitgreen.js
@@ -19,7 +19,7 @@
 		 	}else{
 		 		console.log('write succeed');
 
-		 		child_process.exec("git add -A;GIT_AUTHOR_DATE='" + currentTime + "' GIT_COMMITTER_DATE='" + currentTime + "'git commit -m 'update'; git push;",function(err,opt){
+		 		child_process.exec("git add -A;GIT_AUTHOR_DATE='" + currentTime + "' GIT_COMMITTER_DATE='" + currentTime + "   'git commit -m 'update'; git push;",function(err,opt){
 		 			if(err) throw err;
 		 			console.log("done!!");
 		 		})
