2bdb2b454b5c27da9f3d72db3b882eb9b44831b6
Pearyman <liyananhappy@sina.cn>
2016-08-12 16:53:18 +0800

    update

--- gitgreen.js
+++ gitgreen.js
@@ -8,30 +8,26 @@
  * Version: 1.00
  */
 
-			var fs=require('fs');
-			var child_process=require('child_process');
-			var currentTime=parseInt(new Date().getTime().toString().substr(0,10));
-			// var currentTime=1411963926;
-			var lastTime=currentTime-365*24*60*60;
-			var push=setInterval(function(){
-						currentTime=parseInt(currentTime-86400);
-						fs.writeFile('gitgreen.txt',currentTime,function(err){
-					 	if(err){
-					 		console.log('write failed!');
-					 	}else{
-					 		console.log('write succeed');
-					 		console.log(currentTime);
-					 		child_process.exec("git add .; GIT_AUTHOR_DATE='" + currentTime + "' GIT_COMMITTER_DATE='" + currentTime + "' git commit -m 'update'; git push origin master;",function(err,opt){
-					 			if(err) throw err;
-					 			console.log("done!!");
-					 		})
-					 	}
-					 })
-					},40000);
-			if(currentTime==lastTime){
-				clearInterval(push);
-				console.log("finish!!!")
-			}
-			
-		 	
- 
+var fs = require('fs');
+var child_process = require('child_process');
+var currentTime = parseInt(new Date().getTime().toString().substr(0, 10));
+var lastTime = currentTime - 365 * 24 * 60 * 60;
+var push = setInterval(function() {
+    currentTime = parseInt(currentTime - 86400);
+    fs.writeFile('gitgreen.txt', currentTime, function(err) {
+        if (err) {
+            console.log('write failed!');
+        } else {
+            console.log('write succeed');
+            console.log(currentTime);
+            child_process.exec("git add .; GIT_AUTHOR_DATE='" + currentTime + "' GIT_COMMITTER_DATE='" + currentTime + "' git commit -m 'update'; git push origin master;", function(err, opt) {
+                if (err) throw err;
+                console.log("done!!");
+            })
+        }
+    })
+}, 40000);
+if (currentTime == lastTime) {
+    clearInterval(push);
+    console.log("finish!!!")
+}
